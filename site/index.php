28026b0e50128b58c2f3cb3ed39ec3fc2f845fe7
João Victor Inoue <110576055+joaovictorinoue@users.noreply.github.com>
2023-04-20 22:22:27 -0300

    adiçao do banner e contatos do site

--- site/index.php
+++ site/index.php
@@ -1,40 +1,107 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php
+include "cabecalho.php";
+include "menu.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>iGames</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
-</head>
-
-<body>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">iGame</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<div class="container-fluid banner">
+  <div id="carouselExample" class="carousel slide carousel-fade">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/skyrim.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/residentevill.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/starwars.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/Hogwarts-Legacy.png" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-      </ul>
-      <form action="login.php" method="post" class="d-flex" role="search">
-        <input class="form-control me-2" type="email" placeholder="E-mail">
-        <input class="form-control me-2" type="password" placeholder="Senha">
-        <button class="btn btn-outline-success" type="submit">Entrar</button>
-        <button class="btn btn-outline-success" type="submit">Cadastrar</button>
-      </form>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-12 text-center mt-4">
+      <h1>Jogos em Destaque</h1>
+    </div>
+    <div class="col-12 text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eum tempora aspernatur numquam. Ipsum doloribus, alias deleniti iste magnam cum explicabo itaque ducimus sequi laboriosam neque dolorem odio. Itaque, enim.
+      </h1>
     </div>
   </div>
-</nav>
+  <div class="row">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <?php
+    for ($i = 0; $i < 12; $i++) :
+    ?>
+      <div class="col-md-3 text-center mt-4">
+        <img src="img/skyrim.png" alt="Skyrim" class="img-fluid mb-3">
+        <h3>Skyrim</h3>
+        <a href="nomedojogo.php" class="btn btn-outline-primary mt-3">Ver Mais</a>
+      </div>
+
+    <?php
+    endfor;
+    ?>
+
+    <!-- <div class="col-3 text-center mt-4">
+      <img src="img/residentevill.png" alt="Resident Evil" class="img-fluid mb-3">
+      <h2>Resident Evil 4</h2>
+      <a href="nomedojogo.php" class="btn btn-outline-primary mt-3">Ver Mais</a>
+    </div>
+
+    <div class="col-3 text-center mt-4">
+      <img src="img/starwars.png" alt="Star Wars" class="img-fluid mb-3">
+      <h2>Star Wars Fallen Order</h2>
+      <a href="nomedojogo.php" class="btn btn-outline-primary mt-3">Ver Mais</a>
+    </div>
+
+    <div class="col-3 text-center mt-4">
+      <img src="img/Hogwarts-Legacy.png" alt="Hogwarts Legacy" class="img-fluid mb-3">
+      <h2>Hogwarts Legacy</h2>
+      <a href="nomedojogo.php" class="btn btn-outline-primary mt-3">Ver Mais</a>
+    </div> -->
+
+  </div>
+  <div class="row text-center mt-5">
+    <div class="col-12">
+      <h2>Entre em Contato</h2>
+    </div>
+    <div class="col-12">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti nihil ab nesciunt voluptates, temporibus veniam perferendis ipsam? Debitis impedit eos quaerat totam quibusdam magnam ipsam quo voluptatem, placeat aliquid?</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <form action="inserir-contato.php" method="post">
+      <div class="col">
+        <div class="mb-2">
+          <input type="text" class="form-control" name="nome" placeholder="Nome">
+        </div>
+        <div class="mb-2">
+          <input type="tel" class="form-control" name="telefone" placeholder="Telefone">
+        </div>
+        <div class="mb-2">
+          <textarea class="form-control" name="duvidas" placeholder="Dúvidas" rows="4"></textarea>
+        </div>
+        <div class="mb-2 text-center">
+          <button type="submit" class="btn btn-success w-25 mt-3">Enviar</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ site/jogos.php
@@ -0,0 +1,10 @@
+
+    <?php 
+    include "cabecalho.php";
+    include "menu.php";
+    ?>
+    <div class="container-fluid banner">
+        
+    </div>
+</body>
+</html>
